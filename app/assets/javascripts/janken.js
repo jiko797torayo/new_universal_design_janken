4ca6554fecd896749ff9531d44e9cbc0bb4e504b
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-04-12 20:48:24 +0900

    出し手のランダム化

--- app/assets/javascripts/janken.js
+++ app/assets/javascripts/janken.js
@@ -1,7 +1,17 @@
 $(function(){
+
   document.addEventListener("keydown", function(e) {
     if (e.code === "Space") {
       this.querySelector("a").click();
     }
   });
+
+  //最初の画像以外を非表示にする
+  $(".random a img:gt(0)").hide();
+  //タイマー
+  setInterval(function() {
+       //フェードインとアウトを繰り返す
+      $(".random a :first-child").fadeOut().next("img").fadeIn().end().appendTo(".random a");
+  }, 100);
 });
+
